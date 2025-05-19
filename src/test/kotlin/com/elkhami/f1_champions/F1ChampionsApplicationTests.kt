10ccaf8178c1adbff6c3d920998c622b4daa0d30
Ahmed Elkhami <ahmedeelkhami@gmail.com>
2025-05-19 17:01:21 +0200

    Initial commit

---
+++ src/test/kotlin/com/elkhami/f1_champions/F1ChampionsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.elkhami.f1_champions
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class F1ChampionsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
