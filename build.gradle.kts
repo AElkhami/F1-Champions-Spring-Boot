39517dc4c628474c9f33a01d151565eda524a677
Ahmed Elkhami <ahmedeelkhami@gmail.com>
2025-05-30 21:37:24 +0200

    CI/CD update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	id("org.springframework.boot") version "3.4.5"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("com.diffplug.spotless") version "6.25.0"
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
 	kotlin("plugin.jpa") version "1.9.25"
@@ -74,6 +75,17 @@ dependencies {
 kotlin {
 	compilerOptions {
 		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+spotless {
+	kotlin {
+		target("src/**/*.kt")
+		ktlint("1.2.1")
+	}
+	kotlinGradle {
+		target("*.gradle.kts")
+		ktlint()
 	}
 }
 
